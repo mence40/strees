011cc9838e03b3c99f4f62cb8aa47ce23ffd3281
mence40 <56419133+mence40@users.noreply.github.com>
2021-05-26 15:24:48 -0700

    Update run.py

--- c45/run.py
+++ c45/run.py
@@ -1,6 +1,64 @@
+
+
+
+def classify(tree, data):
+  
+
+
+
+
 from c45 import c45
 import sys
 
 args = sys.argv[1:]
 
-c45(args[0], args[1], args[2])
+tree = c45(args[0], args[1], args[2])
+
+# starts out column wise
+test_data = []
+
+with open(args[3], 'r') as f:
+  for line in f:
+    test_data.append(line.split(","))
+    
+classifications = classify(tree, test_data)
+
+condensed_labels = []
+
+end = len(test_data) - 1
+
+for i in range(len(test_data[0])):
+  if test_data[end][i] == 1:
+    condensed_lables.append(0)
+  else:
+    condensed_lables.append(1)
+    
+correct = 0
+incorrect = 0
+
+for i in range(len(classificaitons)):
+  if classifications[i] == condensed_lables[i]:
+    correct += 1
+  else:
+    incorrect += 1
+    
+    
+print(correct)
+print(incorrect)
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+
